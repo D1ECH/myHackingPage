074a57fa03fc5fd53954266f0fca3ccf05c75b9a
Diego González Rodríguez <diechgr48@gmail.com>
2026-02-25 15:59:12 +0100

    github pages 2

--- astro.config.ts
+++ astro.config.ts
@@ -28,13 +28,14 @@ export default defineConfig({
   //site: 'https://astro-pure.js.org',
   // Deploy to a sub path; See https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   // base: '/astro-pure/',
-  trailingSlash: 'never',
+  // trailingSlash: 'never',
+  trailingSlash: 'ignore',
 
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
   // 1. Vercel (serverless)
-  adapter: vercel(),
-  output: 'server',
+  // adapter: vercel(),
+  // output: 'server',
   // 2. Vercel (static)
   // adapter: vercelStatic(),
   // 3. Local (standalone)
